c7866eda586174116acc8da09d2ed5b7cd7bb62e
PintoDrop <mikebender22@gmail.com>
2023-03-24 10:55:15 -0700

    adding mysql link

--- server.js
+++ server.js
@@ -3,16 +3,26 @@ const PORT = 3001;
 const path = require ("path");
 const fs = require ("fs");
 const inquirer = require("inquirer");
+const express = require("express");
 const console = require("console.table");
 
 
 
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.urlencoded({ extended: true }));
 
-
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "JuiceHead2&&",
+    // change database name once created in mysql
+    database: "classlist_db",
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
 
 
 app.listen(PORT, () => console.log(`Go to http://localhost:${PORT}`));
